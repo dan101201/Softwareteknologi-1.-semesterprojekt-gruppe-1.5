b53510bdb15fec218bfc81fc41ee16034728b9fd
NikolajT <nthom21@student.sdu.dk>
2021-09-21 13:11:10 +0200

    Oprettede variabel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        String status = "Ongoing!";
+        System.out.print("Status er : " + status);
     }
 }

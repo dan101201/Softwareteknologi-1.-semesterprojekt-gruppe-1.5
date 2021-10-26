bc0c44fd116f96082eb31ba52c0c4b89610361c4
NikolajT <nthom21@student.sdu.dk>
2021-10-26 11:54:50 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/worldofzuul/Main.java
+++ src/worldofzuul/Main.java
@@ -7,12 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
 /*
-        Puzzle puzzle = new Puzzle("C:\\Users\\BELille\\Documents\\SDU\\GitHub\\Softwareteknologi-1.-semesterprojekt-gruppe-1.5\\src\\txtfiler\\importantquestions.txt");
+        Puzzle puzzle = new Puzzle("..\\txtfiler\\importantquestions.txt");
         ArrayList<String> answers = new ArrayList<String>();
         answers.add("daniel");
         answers.add("5");
         System.out.println(puzzle.checkAnswers(answers));
 */
 
+*/
     }
 }
--- src/worldofzuul/Player.java
+++ src/worldofzuul/Player.java
@@ -1,4 +1,39 @@
 package worldofzuul;
 
 public class Player {
+    private final Room currentRoom;
+    private int x;
+    private int y;
+
+    public Player(Room currentRoom, int x, int y) {
+        this.currentRoom = currentRoom;
+        this.x = x;
+        this.y = y;
+    }
+
+    // Metoder
+   /* public Puzzle interact(String playerOrientation) {
+        Entity item = currentRoom.Array[x][y+1]; // giver enten entity eller null
+        // Check om det er en entity og så om det er en IoT genstand, om det er outOfBounds,
+        return item
+    } Vi venter til der er lavet noget mere.*/
+
+    // Tag x,y i en switch og ændre værdierne, gem værdierne igen. "Flyt" spilleren i arrayet.
+public void move() {
+        String playerDirection = "Up";
+        switch (playerDirection) {
+            case "Up":
+                y += 1;
+                break;
+            case "Down":
+                y -= 1;
+                break;
+            case "Left":
+                x -= 1;
+                break;
+            case "Right":
+                x += 1;
+                break;
+        }
+    }
 }

2b0269165eb2f6825e7f88d84112950a06a5789e
Sisse <siole21@student.sdu.dk>
2021-10-26 10:50:59 +0200

    gitignore plus made main file a large comment

--- src/worldofzuul/Main.java
+++ src/worldofzuul/Main.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
+/*
         Puzzle puzzle = new Puzzle("C:\\Users\\BELille\\Documents\\SDU\\GitHub\\Softwareteknologi-1.-semesterprojekt-gruppe-1.5\\src\\txtfiler\\importantquestions.txt");
         ArrayList<String> answers = new ArrayList<String>();
         answers.add("daniel");
         answers.add("5");
         System.out.println(puzzle.checkAnswers(answers));
-
+*/
 
     }
 }

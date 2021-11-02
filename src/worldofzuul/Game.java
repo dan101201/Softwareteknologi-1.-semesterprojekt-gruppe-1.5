73355c4698de2d19cb3a4bba27ff69d2a98d6f20
NikolajT <nthom21@student.sdu.dk>
2021-11-02 11:00:49 +0100

    Oprettelse af de rum vi skal have

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -5,13 +5,20 @@ public class Game
     Player player;
     Entity rug;
     public Game() {
-        Room room = new Room(9,9,"The room is large and empty. There is nothing but the walls and floor, both of which is made of cut stone brick");
-        player = new Player(room, 5, 5);
+        Room outside = new Room(9, 6, "Du står ude i din forhave, der er en græsslåmaskine langs sokkelen, og en postkasse ude ved vejen.");
+        Room hall = new Room(5,9,"Gangen er lang og smukt indrettet. Der ligger et ægte persisk tæppe på gulvet, og i hjørnet er kontrolskærmen til dit alarmsystem.");
+        Room kitchen = new Room(9,9,"Fed køkken beskrivelse");
+        Room bath = new Room(5,5,"Lækkert badeværelse");
+        Room homeOffice = new Room (8,6,"Her står der noget om kontoret");
+        Room diningRoom = new Room(9,9,"Og her står der noget om spisestuen");
+        Room bedRoom = new Room(7,7,"Soveværelset her");
+
+        player = new Player(outside, 5, 5);
         rug = new Entity(2, 3, false, "This is a rug", "R");
-        room.getRoomCoordinates()[5][5] = player;
+        outside.getRoomCoordinates()[5][5] = player;
         for (int i=0; i<rug.getWidth(); i++) { // Skal ind i en metode?
             for (int j=0; j<rug.getLength(); j++) {
-                room.getRoomCoordinates()[3+i][3+j] = rug;
+                outside.getRoomCoordinates()[3+i][3+j] = rug;
             }
         }
     }

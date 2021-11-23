69ca020e26a2529ecdddd54339b450b168bdac7f
Sisse <siole21@student.sdu.dk>
2021-11-23 12:39:56 +0100

    lavet sceneskift og menu

--- src/main/java/gui/semesterprojekt/FrontlawnController.java
+++ src/main/java/gui/semesterprojekt/FrontlawnController.java
@@ -3,16 +3,35 @@ package gui.semesterprojekt;
 import javafx.animation.Interpolator;
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Frontlawn implements Initializable {
+public class FrontlawnController implements Initializable {
 // To do: make it possible to run separate controllerClasses
+
+    @FXML
+    private AnchorPane frontlawnroot;
+
+
+    @FXML
+    void moveToHall(ActionEvent event) throws IOException {
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("hall.fxml"));
+        frontlawnroot.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private Button frontdoorButton;
+
     @FXML
     private ImageView lawnmover;
 
--- src/main/java/gui/semesterprojekt/MenuApplication.java
+++ src/main/java/gui/semesterprojekt/MenuApplication.java
@@ -2,17 +2,19 @@ package gui.semesterprojekt;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MenuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        //Parent menuRoot = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Game!");
         stage.setScene(scene);
         stage.show();
     }

7a502033a79b5bd2a3492ea17c473e393fdb3fee
NikolajT <nthom21@student.sdu.dk>
2021-12-06 10:34:12 +0100

    Nu med spillere i flere scener. + Survey after

---
+++ src/main/java/gui/semesterprojekt/SurveyAfterController.java
@@ -0,0 +1,31 @@
+package gui.semesterprojekt;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SurveyAfterController {
+
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label label2;
+
+    @FXML
+    private Label label3;
+
+    @FXML
+    private Button svarKnap;
+
+    @FXML
+    private Label topLabel;
+
+    @FXML
+    void checkAnswers(ActionEvent event) {
+
+    }
+
+}

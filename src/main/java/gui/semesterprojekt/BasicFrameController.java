12596bf7bd2b7733a3297f2f1ac03b839c478410
Daniel Ref <daniel.reffeldt@outlook.com>
2021-12-07 10:45:05 +0100

    set changescene needsplayer parameter to false in basic frame

--- src/main/java/gui/semesterprojekt/BasicFrameController.java
+++ src/main/java/gui/semesterprojekt/BasicFrameController.java
@@ -31,7 +31,7 @@ public class BasicFrameController implements Initializable {
         gameView = gameview;
         MenuApplication.magicLabel = changabletext;
         try {
-            MenuApplication.changeScene("SurveyBefore.fxml", true);
+            MenuApplication.changeScene("SurveyBefore.fxml", false);
         } catch (IOException e) {
             e.printStackTrace();
         }
